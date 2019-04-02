fcd430b7c6036b815737ea0ca7a96a8baf78324e
takesoo <htk.programming@gmail.com>
2019-04-02 15:45:09 +0900

    change_style

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_27_115909) do
+ActiveRecord::Schema.define(version: 2019_04_02_055222) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "text"
     t.integer "user_id"
+    t.string "title"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
